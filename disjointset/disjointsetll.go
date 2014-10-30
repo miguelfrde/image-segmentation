2d44a253684a55f0750102563fa8640b36c6fbd2
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2014-10-30 15:22:44 +0100

    Add DisjointSet LinkedList implementation

---
+++ disjointset/disjointsetll.go
@@ -0,0 +1,126 @@
+package disjointset
+
+/**
+ * Node for the linked list
+ */
+type DisjointSetLLNode struct {
+	id   int
+	next *DisjointSetLLNode
+}
+
+/**
+ * Each component of the disjoint set.
+ * Contains the id of the region, its parent (if the parent has the same
+ * address, it means that it's the root), a pointer to the last element of the
+ * LL, a pointer to the first element of the LL and the size of the region
+ */
+type DisjointSetLLRegion struct {
+	id     int
+	parent *DisjointSetLLRegion
+	head   *DisjointSetLLNode
+	last   *DisjointSetLLNode
+	size   int
+}
+
+/**
+ * DisjointSetLL datatype, it contains a list of all the regions and
+ * the number of components that it's storing
+ */
+type DisjointSetLL struct {
+	regions         []*DisjointSetLLRegion
+	totalComponents int
+}
+
+/**
+ * Instantiates a new DisjointSetLL with 'size' elements
+ */
+func NewDisjointSetLL(size int) *DisjointSetLL {
+	set := new(DisjointSetLL)
+	set.regions = make([]*DisjointSetLLRegion, size, size)
+	for i, _ := range set.regions {
+		set.regions[i] = new(DisjointSetLLRegion)
+		region := set.regions[i]
+		region.id = i
+		region.parent = region
+		region.size = 1
+		region.head = new(DisjointSetLLNode)
+		region.head.id = i
+		region.head.next = region.head
+		region.last = region.head
+	}
+	set.totalComponents = size
+	return set
+}
+
+/**
+ * Return the number of components that are stored
+ */
+func (set *DisjointSetLL) TotalComponents() int {
+	return set.totalComponents
+}
+
+/**
+ * Return the number of elements that are stored
+ */
+func (set *DisjointSetLL) TotalElements() int {
+	return len(set.regions)
+}
+
+/**
+ * Returns the id of the component to which the node v belongs
+ */
+func (set *DisjointSetLL) Find(v int) int {
+	var findNode func(int) *DisjointSetLLRegion
+	findNode = func(v int) *DisjointSetLLRegion {
+		if set.regions[v].parent.id != v {
+			set.regions[v].parent = findNode(set.regions[v].parent.id)
+		}
+		return set.regions[v].parent
+	}
+	return findNode(v).id
+}
+
+/**
+ * Return the size of the region that v belongs to
+ */
+func (set *DisjointSetLL) Size(v int) int {
+	return set.regions[set.Find(v)].size
+}
+
+/**
+ * Merge the regions that a and b belong to
+ */
+func (set *DisjointSetLL) Union(a, b int) {
+	region1 := set.regions[set.Find(a)]
+	region2 := set.regions[set.Find(b)]
+	if region1.id != region2.id {
+		region1.head, region2.last.next = region2.head, region1.head
+		region2.size += region1.size
+		region1.parent = region2
+		set.totalComponents--
+	}
+}
+
+/**
+ * Returns all the elements that belong too the region that v belongs to
+ */
+func (set *DisjointSetLL) Elements(v int) chan int {
+	ch := make(chan int)
+	go func() {
+		node := set.regions[set.Find(v)].head
+		ch <- node.id
+		for node != node.next {
+			ch <- node.next.id
+			node = node.next
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+/**
+ * Returns true if a and b belong to the same region
+ */
+func (set *DisjointSetLL) Connected(a, b int) bool {
+	return set.Find(a) == set.Find(b)
+}
---
+++ disjointset/disjointsetll_test.go
@@ -0,0 +1,51 @@
+package disjointset
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func initSetLL() *DisjointSetLL {
+	set := NewDisjointSetLL(10)
+	set.Union(3, 4)
+	set.Union(1, 3)
+	set.Union(2, 5)
+	set.Union(7, 9)
+	set.Union(0, 5)
+	set.Union(2, 9) // 2-5-9-7-0 3-4-1 6 8
+	return set
+}
+
+func TestInitializationAllDisconnectedLL(t *testing.T) {
+	set := NewDisjointSetLL(5)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			assert.Equal(t, set.Connected(i, j), i == j)
+		}
+	}
+}
+
+func TestUnionLL(t *testing.T) {
+	set := NewDisjointSetLL(5)
+	set.Union(0, 4)
+	assert.True(t, set.Connected(0, 4))
+}
+
+func TestNumberOfComponentsLL(t *testing.T) {
+	set := initSetLL()
+	assert.Equal(t, 4, set.TotalComponents())
+}
+
+func TestComponentSizeLL(t *testing.T) {
+	set := initSetLL()
+	assert.Equal(t, 5, set.Size(2))
+	assert.Equal(t, 5, set.Size(5))
+	assert.Equal(t, 5, set.Size(9))
+	assert.Equal(t, 5, set.Size(7))
+	assert.Equal(t, 5, set.Size(0))
+	assert.Equal(t, 3, set.Size(3))
+	assert.Equal(t, 3, set.Size(4))
+	assert.Equal(t, 3, set.Size(1))
+	assert.Equal(t, 1, set.Size(6))
+	assert.Equal(t, 1, set.Size(8))
+}
